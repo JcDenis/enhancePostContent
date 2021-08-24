c943c60b7ab86bd7883727a20cb7c3f0d2e06c54
Jean-Christian Denis <contact@jcdenis.fr>
2021-08-24 22:55:58 +0200

    update translation

--- locales/fr/main.lang.php
+++ locales/fr/main.lang.php
@@ -1,215 +1,214 @@
 <?php
 // Language: Français 
-// Module: enhancePostContent - 2013.11.08
-// Date: 2013-11-09 05:21:09 
-// Translated with dcTranslater - 2013.05.11 
+// Module: enhancePostContent - 2021.08.24
+// Date: 2021-08-24 20:55:32 
+// Translated with dcTranslater - 2021.08.18 
 
 #_admin.php:24
-#_admin.php:44
-#_widgets.php:43
-#index.php:145
-#index.php:165
+#_admin.php:52
+#_admin.php:100
+#_widgets.php:41
+#index.php:135
+#index.php:155
 $GLOBALS['__l10n']['Enhance post content'] = 'Amélioration du contenu';
 
-#_config.php:26
-#index.php:361
+#_admin.php:75
+#index.php:349
 $GLOBALS['__l10n']['Key'] = 'Clé';
 
-#_config.php:79
-$GLOBALS['__l10n']['Configuration has been successfully updated.'] = 'La configuration a été mise à jour avec succés.';
-
-#_config.php:96
-$GLOBALS['__l10n']['This enable public widgets and contents filter.'] = 'Ceci active les widgets et le filtrage du contenu en partie public.';
-
-#_config.php:100
+#_admin.php:105
 $GLOBALS['__l10n']['Enable plugin'] = 'Activer le plugin';
 
-#_config.php:105
+#_admin.php:107
+$GLOBALS['__l10n']['This enable public widgets and contents filter.'] = 'Ceci active les widgets et le filtrage du contenu en partie public.';
+
+#_admin.php:109
 $GLOBALS['__l10n']['Record list'] = 'Listes d\'enregistrements';
 
-#_config.php:106
+#_admin.php:110
 $GLOBALS['__l10n']['This is the default order of records lists.'] = 'Ceci est l\'ordre de trie par défaut des listes d\'enregistrements.';
 
-#_config.php:114
+#_admin.php:115
 $GLOBALS['__l10n']['Records per page:'] = 'Enregistrements par page :';
 
-#_config.php:121
+#_admin.php:120
 $GLOBALS['__l10n']['This is a special feature to edit list of allowed template values and public pages where this plugin works.'] = 'Ceci est une option spéciale pour modifier les listes des balises de template et des pages publiques ou le plugin sera actif.';
 
-#_config.php:123
+#_admin.php:121
 $GLOBALS['__l10n']['Allowed DC template values:'] = 'Autoriser les balises de template :';
 
-#_config.php:125
+#_admin.php:123
 $GLOBALS['__l10n']['Use "readable_name1:template_value1;readable_name2:template_value2;" like "entry content:EntryContent;entry excerpt:EntryExcerpt;".'] = 'Utiliser "titre1:valeur_de_template1;titre2:valeur_de_template2;" exemple "entry content:EntreyContent;entry excerpt:EntreyExcerpt;".';
 
-#_config.php:127
+#_admin.php:124
 $GLOBALS['__l10n']['Allowed public pages:'] = 'Autoriser les pages publiques :';
 
-#_config.php:129
+#_admin.php:126
 $GLOBALS['__l10n']['Use "readable_name1:template_page1;readable_name2:template_page2;" like "post page:post.html;home page:home.html;".'] = 'Utiliser "titre1:page_de_template1;titre2:page_de_template2;" comme "post page:post.html;home page:home.html;".';
 
-#_widgets.php:46
+#_widgets.php:44
 $GLOBALS['__l10n']['List filtered contents.'] = 'Liste les contenus filtrés.';
 
-#_widgets.php:52
+#_widgets.php:50
 $GLOBALS['__l10n']['In this article'] = 'Dans cet article';
 
-#_widgets.php:87
+#_widgets.php:83
 $GLOBALS['__l10n']['Enable filter on %s'] = 'Autoriser le filtrage sur %s';
 
-#_widgets.php:95
+#_widgets.php:91
 $GLOBALS['__l10n']['Search case insensitive'] = 'Recherche insensible à la casse';
 
-#_widgets.php:102
+#_widgets.php:98
 $GLOBALS['__l10n']['Search also plural'] = 'Recherche également le pluriel';
 
-#_widgets.php:109
+#_widgets.php:105
 $GLOBALS['__l10n']['Show the number of appearance'] = 'Afficher le nombre d\'occurence';
 
-#inc/lib.epc.php:16
+#inc/lib.epc.php:15
 $GLOBALS['__l10n']['Acronym'] = 'Acronyme';
 
-#inc/lib.epc.php:16
+#inc/lib.epc.php:15
 $GLOBALS['__l10n']['Abbreviation'] = 'Abréviation';
 
-#inc/lib.epc.php:16
+#inc/lib.epc.php:15
 $GLOBALS['__l10n']['Definition'] = 'Définition';
 
-#inc/lib.epc.php:17
+#inc/lib.epc.php:16
 $GLOBALS['__l10n']['Citation'] = 'Citation';
 
-#inc/lib.epc.php:17
+#inc/lib.epc.php:16
 $GLOBALS['__l10n']['Replace'] = 'Remplacement';
 
-#inc/lib.epc.php:18
+#inc/lib.epc.php:17
 $GLOBALS['__l10n']['entry excerpt'] = 'le résumé du billet';
 
-#inc/lib.epc.php:18
+#inc/lib.epc.php:17
 $GLOBALS['__l10n']['entry content'] = 'le contenu du billet';
 
-#inc/lib.epc.php:18
+#inc/lib.epc.php:17
 $GLOBALS['__l10n']['comment content'] = 'le contenu des commentaires';
 
-#inc/lib.epc.php:19
+#inc/lib.epc.php:18
 $GLOBALS['__l10n']['home page'] = 'la page d\'accueil';
 
-#inc/lib.epc.php:19
+#inc/lib.epc.php:18
 $GLOBALS['__l10n']['post page'] = 'la page du billet';
 
-#inc/lib.epc.php:19
+#inc/lib.epc.php:18
 $GLOBALS['__l10n']['category page'] = 'la page de catégorie';
 
-#inc/lib.epc.php:19
+#inc/lib.epc.php:18
 $GLOBALS['__l10n']['search results page'] = 'la page des résultats de recherche';
 
-#inc/lib.epc.php:20
+#inc/lib.epc.php:19
 $GLOBALS['__l10n']['atom feeds'] = 'les flux Atom';
 
-#inc/lib.epc.php:20
+#inc/lib.epc.php:19
 $GLOBALS['__l10n']['RSS feeds'] = 'les flux RSS';
 
-#inc/lib.epc.php:101
+#inc/lib.epc.php:100
 $GLOBALS['__l10n']['Highlight tags of your blog.'] = 'Met en avant les Tags de votre blog';
 
-#inc/lib.epc.php:120
+#inc/lib.epc.php:119
 $GLOBALS['__l10n']['Highlight searched words.'] = 'Met en avant les mots recherchés';
 
-#inc/lib.epc.php:139
+#inc/lib.epc.php:138
 $GLOBALS['__l10n']['Explain some acronyms. First term of the list is the acornym and second term the explanation.'] = 'Explique des acronymes. Le premier terme de la liste est l\'acronyme et les second terme l\'explication.';
 
-#inc/lib.epc.php:159
+#inc/lib.epc.php:158
 $GLOBALS['__l10n']['Explain some abbreviation. First term of the list is the abbreviation and second term the explanation.'] = 'Explique des abréviations. Le premier terme de la liste est l\'abréviation et le second terme l\'explication.';
 
-#inc/lib.epc.php:179
+#inc/lib.epc.php:178
 $GLOBALS['__l10n']['Explain some definition. First term of the list is the sample to define and second term the explanation.'] = 'Explique des définitions. Le premier terme de la liste est la chaine à expliquer et le second terme est la définition.';
 
-#inc/lib.epc.php:199
+#inc/lib.epc.php:198
 $GLOBALS['__l10n']['Highlight citation of people. First term of the list is the citation and second term the author.'] = 'Met en avant les citation des gens. Le premier terme est la citation et le second terme est l\'auteur.';
 
-#inc/lib.epc.php:219
+#inc/lib.epc.php:218
 $GLOBALS['__l10n']['Link some words. First term of the list is the term to link and second term the link.'] = 'Ajouter des liens. Le premier terme de la liste est le texte à lier et le second terme est l\'adresse de lien.';
 
-#inc/lib.epc.php:238
+#inc/lib.epc.php:237
 $GLOBALS['__l10n']['Replace some text. First term of the list is the text to replace and second term the replacement.'] = 'Remplace du texte. Le premier terme de la liste est le terme à remplacer et le second le nouveau terme.';
 
-#inc/lib.epc.php:256
+#inc/lib.epc.php:255
 $GLOBALS['__l10n']['Update and show terms. First term of the list is the term to update and second term the new term.'] = 'Met à jour du contenu. Le premier terme de la liste est l\'ancien texte et le second le nouveau texte.';
 
-#inc/lib.epc.php:274
+#inc/lib.epc.php:273
 $GLOBALS['__l10n']['Add link to twitter user page. Every word started with "@" will be considered as twitter user.'] = 'Ajouter un lien vers une page Twitter. Chaque mot commençant par "@" sera considéré comme un nom de compte Twitter';
 
-#inc/lib.epc.php:278
+#inc/lib.epc.php:277
 $GLOBALS['__l10n']['View this user\'s twitter page'] = 'Voir la page Twitter de cet utilisateur';
 
-#index.php:68
-#index.php:90
-#index.php:130
+#index.php:64
+#index.php:85
+#index.php:121
 $GLOBALS['__l10n']['Filter successfully updated.'] = 'Filtre mis à jour.';
 
-#index.php:197
-$GLOBALS['__l10n']['Settings'] = 'Paramètres';
+#index.php:187
+$GLOBALS['__l10n']['Settings'] = 'Réglages';
 
-#index.php:202
+#index.php:192
 $GLOBALS['__l10n']['Pages to be filtered'] = 'Pages à filtrer';
 
-#index.php:220
+#index.php:210
 $GLOBALS['__l10n']['Filtering'] = 'Filtrage';
 
-#index.php:224
+#index.php:214
 $GLOBALS['__l10n']['Case insensitive'] = 'Insensible à la casse';
 
-#index.php:228
+#index.php:218
 $GLOBALS['__l10n']['Also use the plural'] = 'Utiliser également le pluriel';
 
-#index.php:231
+#index.php:221
 $GLOBALS['__l10n']['Limit the number of replacement to:'] = 'Limiter le nombre de remplacement à :';
 
-#index.php:234
+#index.php:224
 $GLOBALS['__l10n']['Leave it blank or set it to 0 for no limit'] = 'Laisser vide ou mettre à 0 pour aucune limite';
 
-#index.php:240
+#index.php:230
 $GLOBALS['__l10n']['Contents to be filtered'] = 'Contenus à filtrer';
 
-#index.php:258
+#index.php:248
 $GLOBALS['__l10n']['Style'] = 'Style';
 
-#index.php:263
+#index.php:253
 $GLOBALS['__l10n']['Class "%s":'] = 'Class CSS "%s" :';
 
-#index.php:274
+#index.php:264
 $GLOBALS['__l10n']['The inserted HTML tag looks like: %s'] = 'Le code HTML de la balise est le suivant: %s';
 
-#index.php:276
+#index.php:266
 $GLOBALS['__l10n']['Ignore HTML tags:'] = 'Ignorer les balises HTML :';
 
-#index.php:279
+#index.php:269
 $GLOBALS['__l10n']['This is the list of HTML tags where content will be ignored.'] = 'Ceci est la liste des balises HTML dont le contenu sera ignoré.';
 
-#index.php:280
+#index.php:270
 $GLOBALS['__l10n']['Tag "%s" always be ignored.'] = 'Les balises "%s" seront toujours ignorées.';
 
-#index.php:343
+#index.php:332
 $GLOBALS['__l10n']['&#171;prev.'] = 'préc.';
 
-#index.php:344
+#index.php:333
 $GLOBALS['__l10n']['next&#187;'] = 'suiv.';
 
-#index.php:349
+#index.php:338
 $GLOBALS['__l10n']['Records'] = 'Enregistrements';
 
-#index.php:352
+#index.php:341
 $GLOBALS['__l10n']['No record'] = 'Pas d\'enregistrement';
 
-#index.php:387
+#index.php:375
 $GLOBALS['__l10n']['In order to remove a record, leave empty its key or value.'] = 'Pour retirer un enregistrement, laisser vide sa clé ou sa valeur.';
 
-#index.php:406
+#index.php:394
 $GLOBALS['__l10n']['New record'] = 'Nouvel enregistrement';
 
-#index.php:409
+#index.php:397
 $GLOBALS['__l10n']['Key:'] = 'Clé :';
 
-#index.php:413
+#index.php:401
 $GLOBALS['__l10n']['Value:'] = 'Valeur :';
 
-?>
+$GLOBALS['__l10n']['Configuration has been successfully updated.'] = 'La configuration a été mise à jour avec succés.';
+
